86a373a7f690e4f0e7c6f7b55dc013aeef2c381a
kushagrakatiha <kushagrakatiha123@gmail.com>
2024-07-13 13:35:26 +0530

    complete boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='bg-black text-white text-4xl text-center'>Redux ToolKit Revision</h1>
+    </>
+  )
+}
+
+export default App
